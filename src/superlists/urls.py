21da3cd76eb698739fd9d6277da9d6c4d3f3030a
Shreyas Mondikal Subramanya <mondikal@atri.io>
2023-09-21 13:47:56 -0700

    test: TDD Tutorial
    Completed Chapter 4 of TDD Tutorial
    
    [ICTRAINING-1574]

--- src/superlists/urls.py
+++ src/superlists/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import sys
+sys.path.append("..")
 
 from django.urls import path
-from ..lists.views import home_page
+from src.lists.views import home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
