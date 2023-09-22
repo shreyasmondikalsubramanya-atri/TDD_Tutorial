d46826121cc75825f0505668e44aa8f00642d3f0
Shreyas Mondikal Subramanya <mondikal@atri.io>
2023-09-22 13:27:41 -0700

    test: TDD Tutorial
    Completed Chapter 5 of TDD Tutorial
    
    [ICTRAINING-1575]

---
+++ src/lists/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+def home_page(request):
+    if request.method == "POST":
+        Item.objects.create(text=request.POST["item_text"])
+        return redirect("/")
+
+    items = Item.objects.all()
+    return render(request, "home.html", {"items": items})
