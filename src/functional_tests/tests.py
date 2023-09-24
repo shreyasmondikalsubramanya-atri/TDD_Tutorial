1364f3cf218a5aa7e9736703d54f8126d004f657
Shreyas Mondikal Subramanya <mondikal@atri.io>
2023-09-24 14:20:26 -0700

    test: TDD Tutorial
    Completed Chapter 7 of TDD Tutorial
    
    [ICTRAINING-1577]

--- src/functional_tests/tests.py
+++ src/functional_tests/tests.py
@@ -1,86 +1,103 @@
-import unittest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 from django.test import LiveServerTestCase
 
+MAX_WAIT = 10
+
+
 class NewVisitorTest(LiveServerTestCase):
-    def setUp(self):  
-        self.browser = webdriver.Firefox()  
 
-    def tearDown(self):  
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
         self.browser.quit()
-
-    def test_can_start_a_todo_list(self):
-        # Edith has heard about a cool new online to-do app.
-        # She goes to check out its homepage
-        self.browser.get(self.live_server_url)
-
-        # She notices the page title and header mention to-do lists
-        self.assertIn("To-Do", self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
-        self.assertIn("To-Do", header_text)
-
-        # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
-        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
-
-        # She types "Buy peacock feathers" into a text box
-        # (Edith's hobby is tying fly-fishing lures)
-        inputbox.send_keys("Buy peacock feathers")
-
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
-
-        # There is still a text box inviting her to add another item.
-        # She enters "Use peacock feathers to make a fly"
-        # (Edith is very methodical)
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table("1: Buy peacock feathers")
-
-        # There is still a text box inviting her to add another item.
-        # She enters "Use peacock feathers to make a fly"
-        # (Edith is very methodical)
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
-        inputbox.send_keys("Use peacock feathers to make a fly")
-        inputbox.send_keys(Keys.ENTER)
-
-        # The page updates again, and now shows both items on her list
-        self.wait_for_row_in_list_table("1: Buy peacock feathers")
-        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
-
-        # Satisfied, she goes back to sleep
-
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn(row_text, [row.text for row in rows])
-
-    def test_can_start_a_todo_list(self):
-        [...]
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
             try:
-                table = self.browser.find_element(By.ID, "id_list_table")
-                rows = table.find_elements(By.TAG_NAME, "tr")
+                table = self.browser.find_element('id', 'id_list_table')
+                rows = table.find_elements('tag name', 'tr')
                 self.assertIn(row_text, [row.text for row in rows])
                 return
-            except (AssertionError, WebDriverException):
+            except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
-                    raise
+                    raise e
                 time.sleep(0.5)
 
+    def test_start_list_and_retreive_later(self):
+        # To checkout homepage
+        self.browser.get(self.live_server_url)
 
-if __name__ == "__main__":
-    unittest.main()  
+        # To-do in the header of the web page
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element('tag name', 'h1').text
+        self.assertIn('To-Do', header_text)
+
+        # Entering a to-do item
+        input_box = self.browser.find_element("id", 'id_new_item')
+        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # Enter "buy-peacock-feathers" in text box
+        input_box.send_keys('Buy peacock feathers')
+
+        # On hitting enter, the page updates, and the page lists "1: Buy peacock feathers" as an item in a to-do list
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # Use textbox to add another to-do "Use peacock feathers to make a fly"
+        input_box = self.browser.find_element("id", 'id_new_item')
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # Page updates and shows both the items on the list
+
+        # Unique url is generated for the user and there is an explanatory text to that effect
+
+        # Visiting the unique url just to see if its still there
+
+        # Goes back to sleep
+
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element('id', 'id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # Now, new user comes along
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # visiting home page
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element('tag name', 'body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # starts a list
+        inputbox = self.browser.find_element('id', 'id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # new url
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # No trace of other user's list
+        page_text = self.browser.find_element('tag name', 'body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'tutorial_db',
         'USER': 'postgres',
-        'PASSWORD': 'aDanceWithDragons#7',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
